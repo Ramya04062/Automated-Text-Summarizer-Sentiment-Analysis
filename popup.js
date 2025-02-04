c2b05d70f9d052ca5ce8cf44359c0196c2b8be32
Ramya K <132529065+Ramya04062@users.noreply.github.com>
2025-02-04 22:07:25 +0530

    Delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    chrome.storage.local.get(['summary'], (result) => {
-      document.getElementById('summary').innerHTML = result.summary || 'No summary available';
-    });
-  });
-  
-  
-  
